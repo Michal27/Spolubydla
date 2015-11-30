b5e4219155467ba255930e7235aaa64661be1ead
Michal Malaník <michal.malanik@closeit.cz>
2015-11-30 14:24:53 +0100

    Tisknuti datumu

--- app/src/main/java/cz/example/innovasoft/spolubydla/MainActivity.java
+++ app/src/main/java/cz/example/innovasoft/spolubydla/MainActivity.java
@@ -70,6 +70,7 @@ public class MainActivity extends AppCompatActivity
         allTasks = new ArrayList<Task>();
         code = new String();
         actualMember= new Member();
+        actualTask.due_date = new MyDate();
 
         if(userId == -1) {
             startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
@@ -296,6 +297,9 @@ public class MainActivity extends AppCompatActivity
             TextView taskPoints = (TextView) square.findViewById(R.id.taskPoints);
             taskPoints.setText(displayTasks.get(i).getPoints());
 
+            TextView taskDate = (TextView) square.findViewById(R.id.taskDate);
+            taskDate.setText(displayTasks.get(i).getDueDate().getDay() + "." + displayTasks.get(i).getDueDate().getMonth() + "." + displayTasks.get(i).getDueDate().getYear());
+
             ImageView userColor = (ImageView) square.findViewById(R.id.userImage);
             if(actualMember.getColor().equals(Integer.toString(0)))//user.color == 0
             {
@@ -329,6 +333,8 @@ public class MainActivity extends AppCompatActivity
             {
                 userColor.setColorFilter(Color.rgb(0,0,0)); //black
             }
+
+
 
             mainContent.addView(square);
             mainContent.invalidate();
